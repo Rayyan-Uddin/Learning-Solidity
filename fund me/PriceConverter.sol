50e43c5b320b344436ca91fe18744e5d8907c08a
Rayyan Uddin <rayyanuddin007@gmail.com>
2024-09-10 23:22:25 +0500

    Update PriceConverter.sol

--- fund me/PriceConverter.sol
+++ fund me/PriceConverter.sol
@@ -4,6 +4,7 @@ pragma solidity 0.8.26;
 /*
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
+// all functions of library should be internal
 library PriceConverter{
 
     function getPrice() internal view returns(uint256) {
